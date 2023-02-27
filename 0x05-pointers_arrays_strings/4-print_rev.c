f0852bc5963d464348464d246cea438bb8219c99
Abdulmudallib <aik200772@gmail.com>
2023-02-27 13:10:51 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
- * print_rev - Prints a string in reverse.
- * @s: string to be reserved.
+ *reverse_array - reverses an array of integers
+ *@a: array to be reversed
+ *@n: number of element in the array
  */
-void print_rev(char *s)
-{
-	int len = 0, i;
+ voidreverse_array(int *a, int n)
+ {
+        int i, j, tmp;
 
-	while (s[i++])
-		len++;
+        j = n - 1;
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
-
-	_putchar('\n');
+        for (i = 0; < n/2; i++)
+        {
+        tmp = a[i];
+        a[i] = a[j];
+        a[j--] = tmp;
+        }
 }
+
